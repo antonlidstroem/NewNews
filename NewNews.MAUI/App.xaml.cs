b3db0725ec15abecf538449e32acf47b7f047f9c
Anton Lidström <antonlidstroem@outlook.com>
2026-02-05 10:04:56 +0100

    Colors and buttons bettter

--- NewNews.MAUI/App.xaml.cs
+++ NewNews.MAUI/App.xaml.cs
@@ -7,6 +7,7 @@ namespace NewNews.MAUI
         public App()
         {
             InitializeComponent();
+            //UserAppTheme = AppTheme.Dark;
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
